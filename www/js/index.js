3a629c6e85ac3d769c72253260482017543be855
involved1984 <behzad1122@hotmail.com>
2017-07-01 22:56:45 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -41,6 +41,30 @@ var app = {
         }); */
         
         
+        /****************************/
+        ar push = PushNotification.init({ "android": {"senderID": "812663353807"},
+             "ios": {"alert": "true", "badge": "true", "sound": "true"}, "windows": {} } );
+
+        push.on('registration', function(data) {
+            // data.registrationId
+        });
+
+        push.on('notification', function(data) {
+            // data.message,
+            // data.title,
+            // data.count,
+            // data.sound,
+            // data.image,
+            // data.additionalData
+        });
+
+        push.on('error', function(e) {
+            // e.message
+        });
+        /****************************/
+        
+        
+        
         receivedElement.innerHTML = 'Waiting for notification token';
         
         
