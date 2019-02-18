035fe3e48ca0149b2af6b277aaeb7a74aeca4086
involved1984 <behzad1122@hotmail.com>
2019-02-19 00:53:29 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -9,20 +9,35 @@ var app = {
         app.receivedEvent('deviceready');
     },
     receivedEvent: function(id) {
-        LoadingAndInitializingTheApp();
+        selectServer();
         
+         $('#serverURLButton').on('click', function(){
+            window.localStorage.setItem("serverurl", $('#serverURL').val());
+            selectServer();
+        });
     }
 };
 
+var SERVER_URL = '';
+function selectServer(){
+    if (window.localStorage.getItem("serverurl").length > 0){
+        SERVER_URL = window.localStorage.getItem("serverurl");
+        $('#serverUrlContainer').hide();
+        $('#globalLoaderHolder').show();
+        LoadingAndInitializingTheApp();
+    } else {
+       $('#serverUrlContainer').show();
+    }
+}
 
-var loaderAlignInterval;
 function LoadingAndInitializingTheApp(){
         
     
         ajaxObject = $.ajax({
             type: 'POST',
             //url: 'http://192.168.1.131/matbutik/inventory/index.php',
-            url: 'https://www.matbutik.se/inventory/index.php',
+            //url: 'https://www.matbutik.se/inventory/index.php',
+            url: SERVER_URL + '/inventory/index.php',
             data: {},
             success: function(data){
                 $('#pageContainer').html('');
@@ -30,7 +45,7 @@ function LoadingAndInitializingTheApp(){
             },
             error : function(){
                 setTimeout(function(){
-                    $('#pageContainer').html('<center><br><br><br>Error loading<br>Retrying...</center>');
+                    $('#errorLoadingAppMessage').show();
                     LoadingAndInitializingTheApp();
                 }, 1000);
             },
